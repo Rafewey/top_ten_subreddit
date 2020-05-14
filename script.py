d1c8a044937dd5a2fff58ab965bb27a39f672935
rafewey <rafewey@gmail.com>
2020-05-14 05:38:35 -0700

    Fixed the requirements.txt file and added a pause to script.py every hour

--- script.py
+++ script.py
@@ -1,1 +1,26 @@
 from toptensubreddit import TopTenSubReddit
+import time
+
+print("IN ORDER TO GRAB THE TOP 10 POSTS FROM YOUR DESIERED SUBREDDIT, YOU MUST FIRST HAVE THE CORRECT CREDENTIALS")
+
+user_username = input("YOUR REDDIT USERNAME")
+user_password = input("YOUR REDDIT PASSWORD")
+user_app_name = input("YOUR APP NAME")
+user_personal_use_script = input("YOUR PERSONAL SCRIPT KEY")
+user_secret = input("YOUR SECRET KEY")
+user_subreddit = input("WHAT SUBREDDIT WOULD YOU LIKE TO SCRAPE FROM?")
+
+#remove any whitespaces from the left and right of the strings that were inputted
+user_username.strip()
+user_password.strip()
+user_app_name.strip()
+user_personal_use_script.strip()
+user_secret.strip()
+
+#login to reddit with appropiate credentials
+login = TopTenSubReddit(user_subreddit, user_username, user_password, user_personal_use_script,
+user_secret, user_app_name)
+
+while True:
+    login.make_spreadsheet()
+    time.sleep(3600)
